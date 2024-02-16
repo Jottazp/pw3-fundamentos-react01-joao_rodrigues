ad1bf0216bf703bd2220a5d48453e7c8ee821d72
Jottazp <contato.rodrigues5000@gmail.com>
2024-02-15 22:39:44 -0300

    Joao_Rdrigues: inicio do css do cardBook

--- fundamentos-react-01-joao_rodrigues/src/App.js
+++ fundamentos-react-01-joao_rodrigues/src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h3>As Cavernas de Aço</h3>
 
-      <p>Isaac Asimov</p>
+      <div className='cardBook'>
+        <h3>As Cavernas de Aço</h3>
 
-      <img src={cavernas} width={150} alt='Capa do livro as cavernas de aço Isaac Asimov'/>
+        <p>Isaac Asimov</p>
 
-      <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.</p>
+        <img src={cavernas} width={150} alt='Capa do livro as cavernas de aço Isaac Asimov'/>
 
+        <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.</p>
+
+      </div>
     </div>
   );
 }
