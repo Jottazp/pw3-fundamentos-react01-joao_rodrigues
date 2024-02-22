02b323efe7272fb9ad66654c293f92abbd7f67a6
Jottazp <contato.rodrigues5000@gmail.com>
2024-02-22 20:27:15 -0300

    Componentização de livros

--- fundamentos-react-01-joao_rodrigues/src/App.js
+++ fundamentos-react-01-joao_rodrigues/src/App.js
@@ -1,21 +1,36 @@
 import logo from './logo.svg';
-import cavernas from './livros/cavernas_aco.jpg';
+import Livro from './components/Livro';
 import './App.css';
+
+import cavernas from './livros/cavernas_aco.jpg';
+import sol from './livros/sol_desvelado.jpg';
+import alvorada from './livros/robos_alvorada.jpg';
+
+const desc = 'As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Livro
+        titulo="As Cavernas de Aço"
+        autor="Isaac Asimov"
+        imagem={cavernas}
+        descricao={desc}
+      />
 
-      <div className='cardBook'>
-        <h3>As Cavernas de Aço</h3>
+      <Livro
+        titulo="Sol Desvelado"
+        autor="Isaac Asimov"
+        imagem={sol}
+        descricao={desc}
+      />
 
-        <p>Isaac Asimov</p>
-
-        <img src={cavernas} width={150} alt='Capa do livro as cavernas de aço Isaac Asimov'/>
-
-        <p>As Cavernas de Aço, de Isaac Asimov, é o primeiro romance da consagrada Série dos Robôs, uma das mais populares da ficção científica. A história nasceu de um desafio: Asimov queria provar para seu editor que a ficção científica não era limitada e poderia ser incorporada a qualquer gênero literário, inclusive nos dramas policiais.</p>
-
-      </div>
+      <Livro
+        titulo="Robôs da Alvorada"
+        autor="Isaac Asimov"
+        imagem={alvorada}
+        descricao={desc}
+      />
     </div>
   );
 }
